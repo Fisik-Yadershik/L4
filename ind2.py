0f35bb3e0123b493e01d56978b4fa30a3a6b14af
Fisik-Yadershik <v.aaaaaa2014@yandex.ru>
2020-11-03 00:23:54 +0300

    country in depression

--- ind2.py
+++ ind2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#Составить программу, выясняющую делится ли натуральное число нацело на натуральное число .
+
 a = int(input("First"))
 b = int(input("Second"))
 if a%b==0:
--- ind3.py
+++ ind3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#Дано натуральное число . Получить все его натуральные делители.
+
 x = int(input("Chislo"))
 a = int(1)
 while a<=x:
